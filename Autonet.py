ee36152c7f26df867a38b03930151791de9b9273
kbeliasas <karolis.beliasas@gmail.com>
2018-05-28 11:57:54 +0300

    UP

--- Autonet.py
+++ Autonet.py
@@ -163,6 +163,39 @@ def remove_mac(host,user,password,mac,vlan):
         logging.basicConfig(level=logging.ERROR)
         remove_mac_second(host,user,password, mac, vlan)
 
+def draw_topology(graph, labels=None, graph_layout='shell',
+               node_size=1600, node_color='blue', node_alpha=0.3,
+               node_text_size=12,
+               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+               edge_text_pos=0.3,
+               text_font='sans-serif'):
+    G=nx.Graph()
+
+    for edge in graph:
+        G.add_edge(edge[0], edge[1])
+
+    if graph_layout == 'spring':
+        graph_pos=nx.spring_layout(G)
+    elif graph_layout == 'spectral':
+        graph_pos=nx.spectral_layout(G)
+    elif graph_layout == 'random':
+        graph_pos=nx.random_layout(G)
+    else:
+        graph_pos=nx.shell_layout(G)
+
+
+    nx.draw_networkx_nodes(G, graph_pos, node_size=node_size, alpha=node_alpha, node_color=node_color)
+    nx.draw_networkx_edges(G, graph_pos, width=edge_tickness, alpha=edge_alpha, edge_color=edge_color)
+    nx.draw_networkx_labels(G, graph_pos, font_size=node_text_size, font_family=text_font)
+
+    if labels is None:
+        labels = range(len(graph))
+
+    edge_labels = dict(zip(graph, labels))
+    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, label_pos=edge_text_pos)
+
+    plt.show()
+
 
 
 
@@ -300,14 +333,14 @@ while True:
                 print "miss"
 
 
-for x in range(0, len(manage)):
-    for y in range(0, len(all_list[x])):
-        temp = (manage[x], all_list[x][y])
-        realations.append(temp)
+    for x in range(0, len(manage)):
+        for y in range(0, len(all_list[x])):
+            temp = (manage[x], all_list[x][y])
+            realations.append(temp)
 
 
 
-draw_topology(realations,all_ports_topology)
+    draw_topology(realations,all_ports_topology)
 
 
     for list1 in added_macs:
